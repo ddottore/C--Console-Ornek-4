a24616ad27d4a15c5ec2a7b60e36ac31db65f79c
Emirhan Çalışkan <emrhncalskn@gmail.com>
2022-02-16 22:04:41 +0300

    Faktöriyel sayıların faktöriyel biçimde toplanması, örn: 5 sayısı >> 5! + 4! + 3! + 2! + 1!

---
+++ console örnek4/Program.cs
@@ -0,0 +1,20 @@
+﻿int sayi, i , x , carpim = 1 , toplam = 0;
+Console.Write("Faktöriyel Toplamın yapılacağı sayıyı giriniz : ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+for (x=1; x <= sayi;x++) 
+{
+ 
+    for (i = x; i <= x; i++)
+    {
+        carpim *= i;
+    }
+
+    toplam += carpim;
+}
+
+Console.WriteLine("Cevap : " + toplam);
+
+
+
+//Faktöriyel sayıların faktöriyel biçimde toplanması, örn: 5 sayısı >> 5! + 4! + 3! + 2! + 1!
